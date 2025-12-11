69e26a30a406ab896c2a08796bfb093dcd1b13dd
Erick Francisco Merino Menares <erick.merino@mineduc.cl>
2025-12-11 14:50:55 -0300

    fix goldsteinprice formula and make a test (parenthesis error)

--- src/anmetal/problems/nonlinear_functions/two_inputs.py
+++ src/anmetal/problems/nonlinear_functions/two_inputs.py
@@ -30,7 +30,7 @@ class Goldsteinprice:
     @staticmethod
     def func_simple(x: float, y:float) -> float:
         result = 1 + ((x+y+1)**2) * (19-14*x + 3*(x**2)-14*y + 6*x*y + 3*(y**2))
-        result = result * ((30+ (2*x - 3*y)**2) * 18-32*x+ 12*(x**2) + 48*y - 36*x*y + 27*(y**2) )
+        result = result * (30 + ((2*x - 3*y)**2) * (18 - 32*x + 12*(x**2) + 48*y - 36*x*y + 27*(y**2)))
         return result
 
     @staticmethod
